013ee45f8d4358ad461d613058cb6d64a4c74771
Ole Magnus Fon Johnsen <ole.magnus@me.com>
2022-08-28 19:43:33 +0200

    remove noborder patch

--- autostart.sh
+++ autostart.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 lxpolkit &
-pgrep emacs || emacs --daemon &
+#pgrep emacs || emacs --daemon &
 picom --experimental-backends &
 xsetroot -cursor_name left_ptr &
 $HOME/.fehbg &
 dwmblocks
+mount-drives
